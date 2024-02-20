6c731c7c6c8aeedc53e4d6299652f199abfef00e
nickmasonswe <nickmasonswe@gmail.com>
2024-02-20 10:28:07 -0500

    groupAnagrams ts

--- neetCode/groupAnagrams.ts
+++ neetCode/groupAnagrams.ts
@@ -35,4 +35,25 @@ Constraints:
 
 */
 
-//Ran out of time two blocks in a row. Peeked at solution. Will continue this next 20 minute block
+function groupAnagrams(strs: string[]): string[][] {
+    //init an hashtable where key->sorted letters, val->each word that contains these letters
+    const hash = {};
+    //loop through the array and sort the letters in each word 
+    for(const word of strs) {
+        const sorted = word.split("").sort().join("");
+    //check if the hash already has a key that matches the sorted version of the current word
+        //if so, push the unsorted word to the array associated with the sorted key
+        if(hash[sorted]) {
+            hash[sorted].push(word);
+            //if not, assign an array containing that unsorted word to the associated sorted key
+        } else {
+            hash[sorted] = [word];
+        }
+    }
+    //return an array containing all of the values from the hash
+    return Object.values(hash);
+};
+
+//Settled on the sorting method of solving this problem. 
+//Time: O(m * n logn) (m being the length of the array)
+//Space: O(m * n)
