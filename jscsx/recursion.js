ea3ef9d5270e7f59188a85b51a88f83110822fee
nickmasonswe <nickmasonswe@gmail.com>
2024-03-26 10:44:38 -0400

    pow jscsx

--- jscsx/recursion.js
+++ jscsx/recursion.js
@@ -46,3 +46,23 @@ console.log(getLength([1, 2, 3, 4, 5])); // -> 5
 console.log(getLength([])); // -> 0
 
 /* ------------------------ ------------------------ */
+//Write a function that takes two inputs, a base and an exponent, and returns the expected value of base ^ exponent. For instance, if our base is 2 and our exponent is 3, then return 8 because 2^3 = 2*2*2 = 8.
+
+function pow(base, exponent, count = 0, result = 1) {
+  //base case
+  if (base === 0) return 1;
+  if (base === 1) return base;
+  if (count === exponent) return result;
+  //recursive case
+  result *= base;
+  return pow(base, exponent, ++count, result);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(pow(2, 4)); // -> 16
+console.log(pow(3, 5)); // -> 243
+/* ------------------------ ------------------------ */
+
+/* ------------------------ ------------------------ */
+
+/* ------------------------ ------------------------ */
