3850d26d81af7502cbdd28649f142ded449efe09
nickmasonswe <nickmasonswe@gmail.com>
2024-03-26 10:39:08 -0400

    factorial jscsx

--- jscsx/recursion.js
+++ jscsx/recursion.js
@@ -12,3 +12,23 @@ function repeater(char) {
 // To check if you've completed the challenge, uncomment these console.logs!
 console.log(repeater('g'));
 console.log(repeater('j'));
+
+/* ------------------------ ------------------------ */
+//Write a function that returns the factorial of a number.
+
+//EXAMPLE4! = 4 * 3 * 2 * 1 = 24, so calling factorial(4) should return 24.
+
+function factorial(num, result = 1) {
+  //base case
+  if (num - 1 === 0) return result;
+  //recursive case
+  result *= num;
+  return factorial(num - 1, result);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(factorial(4)); // -> 24
+console.log(factorial(6)); // -> 720
+/* ------------------------ ------------------------ */
+
+/* ------------------------ ------------------------ */
