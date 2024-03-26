881f05b29b8680ecb358040d7bfe67c036d678f9
nickmasonswe <nickmasonswe@gmail.com>
2024-03-26 10:48:23 -0400

    shuffleCards jscsx

--- jscsx/recursion.js
+++ jscsx/recursion.js
@@ -88,5 +88,54 @@ function subtract10(num) {
 const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
 console.log(flow(2, arrayOfFunctions)); // -> -7
 /* ------------------------ ------------------------ */
+/* 
+You are creating a card game application with your friend.
 
+She already wrote a function that divides the deck of cards into top and bottom halves, but needs help writing a function that shuffles the two halves together again.
+Challenge
+
+Write a function that takes two arrays as inputs, representing the top and bottom halves of a deck of cards, and shuffles them together. The function will return a single array containing the elements from both input arrays interleaved, like so:
+
+the first element should be the first element of the first input array,
+
+the second element should be the first element of the second input array,
+
+the third element should be the second element of the first input array,
+
+the fourth element should be the second element of the second array,
+
+and so on.
+
+The arrays may be of different lengths. After interleaving the elements of the input arrays, any remaining elements should be appended to the end of the array.
+
+This problem can be solved in many ways, but try to solve it with recursion!
+*/
+
+function shuffleCards(topHalf, bottomHalf, shuffledDeck = []) {
+  // YOUR CODE HERE
+  //base case
+  if (!topHalf[0] && !bottomHalf[0]) return shuffledDeck;
+  if (!topHalf[0] && bottomHalf[0]) return shuffledDeck.concat(bottomHalf);
+  if (topHalf[0] && !bottomHalf[0]) return shuffledDeck.concat(topHalf);
+  //recursive case
+  shuffledDeck.push(topHalf[0], bottomHalf[0]);
+  return shuffleCards(topHalf.slice(1), bottomHalf.slice(1), shuffledDeck);
+}
+
+// UNCOMMENT TO TEST YOUR WORK
+const topHalf = [
+  'Queen of Diamonds',
+  'Five of Hearts',
+  'Ace of Spades',
+  'Eight of Clubs',
+];
+const bottomHalf = ['Jack of Hearts', 'Ten of Spades'];
+console.log(shuffleCards(topHalf, bottomHalf));
+/*-> ['Queen of Diamonds',
+        'Jack of Hearts',
+        'Five of Hearts',
+        'Ten of Spades',
+        'Ace of Spades',
+        'Eight of Clubs',
+      ]
 /* ------------------------ ------------------------ */
