0a7e4cfabf7aa926a0274294062c0653eca384ed
nickmasonswe <nickmasonswe@gmail.com>
2024-05-08 16:13:46 -0500

    refactor a loop to list comprehension python DSA course

---
+++ python/dsa_in_python_course/list_comprehension_refactor.py
@@ -0,0 +1,27 @@
+class TreeNode:
+  def __init__(self, value):
+    self.value = value
+    self.children = []
+
+  def add_child(self, child_node):
+    print("Adding " + child_node.value)
+    self.children.append(child_node)
+    
+  def remove_child(self, child_node):
+    print("Removing " + child_node.value + " from " + self.value)
+    new_children = [ child for child in self.children if child != child_node]
+    self.children = new_children
+    #new_children = []
+    #for child in self.children:
+    #  if child != child_node:
+    #    new_children.append(child)
+    #self.children = new_children
+
+root = TreeNode("I am Root")
+child = TreeNode("A wee sappling")
+bad_seed = TreeNode("Root Rot!")
+
+root.add_child(child)
+root.add_child(bad_seed)
+
+root.remove_child(bad_seed)
