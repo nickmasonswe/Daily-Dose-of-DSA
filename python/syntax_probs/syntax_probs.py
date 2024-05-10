4061ec6d8c683a6236f4d6cdda71ab9ea03ad6c4
nickmasonswe <nickmasonswe@gmail.com>
2024-05-10 16:46:44 -0400

    add a few questions to do during my flight

--- python/syntax_probs/syntax_probs.py
+++ python/syntax_probs/syntax_probs.py
@@ -28,3 +28,13 @@ def divisible_recursive(num,arr):
 
 print(divisible_recursive(3,[9,18,6])) #True
 print(divisible_recursive(3,[10,18,6])) #False
+
+#Create a functon "buildSentence" that takes any number of words (strings) and returns a complete sentence with the first word capitalizated and punctuation (add an exclamation point to each sentence).
+
+# Declare a function 'calculate', which takes in a number, initialValue, as a single argument and returns another function.
+# The returned function takes up to 2 arguments, a string (it could be any of 4 basic operators: '+', '-', '*', and '/'), a number, or just a single argument. When invoked with two arguments, return the message "Press enter '=' to see the result!". When the returned function is invoked with just an equal sign ('='), return the evaluated result from all previous expressions (starting at the initialValue). 
+ # Note: Do not use the 'eval()' method in JS!
+
+#Get the length of an array using recursion without accessing its length property.
+
+#/Write a function that takes an array of functions and a number that will be piped through all those functions. The input number passes through the first function, whose output is passed as input to the second function, whose output is passed as input to the third function, and so on. Use recursion to return the final output of the last function in the array.
