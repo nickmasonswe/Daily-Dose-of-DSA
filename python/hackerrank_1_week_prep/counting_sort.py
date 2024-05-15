24cd2ad97ec1996c23ae57327bd04ce01648668e
nickmasonswe <nickmasonswe@gmail.com>
2024-05-15 10:16:53 -0400

    counting_sort.py

---
+++ python/hackerrank_1_week_prep/counting_sort.py
@@ -0,0 +1,18 @@
+""" Note
+For this exercise, always return a frequency array with 100 elements. The example above shows only the first 4 elements, the remainder being zeros.
+
+Challenge
+Given a list of integers, count and return the number of times each value appears as an array of integers. """
+
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Create a frequency array
+    freq_arr = [0] * 100
+    for el in arr:
+        freq_arr[el] += 1
+    return freq_arr
